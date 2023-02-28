d6e0bfca5fd4310ade55ced089fafe5a6ca2ce0b
Ren Besson <ren.besson@outlook.com>
2023-02-28 16:37:26 -0400

    Team Builder done

--- index.js
+++ index.js
@@ -2,18 +2,80 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const questions = require('./src/questions');
 const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 
-const init = async () => {
+const team = [];
+nextAction = null;
+
+const addManagerInquirer = async () => {
   try {
-    const answers = await inquirer.prompt(questions);
-    console.log(answers);
+    const answers = await inquirer.prompt(questions.managerQuestions);
+    const manager = new Manager(answers);
+    team.push(manager);
   } catch (error) {
     console.error(error);
   }
 };
 
-//init();
+///////////////////////////////////////////////////////////////////////////////
+// Add Engineer
+///////////////////////////////////////////////////////////////////////////////
+const addEngineerInquirer = async () => {
+  try {
+    const answers = await inquirer.prompt(questions.engineerQuestions);
+    const newEngineer = new Engineer(answers);
+    team.push(newEngineer);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-const newEmployee = new Manager('joao silva', 21212, 'joao.silva@email.com.br', 12);
+///////////////////////////////////////////////////////////////////////////////
+// Add Intern
+///////////////////////////////////////////////////////////////////////////////
+const addInternInquirer = async () => {
+  try {
+    const answers = await inquirer.prompt(questions.internQuestions);
+    const newIntern = new Intern(answers);
+    team.push(newIntern);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-console.log(newEmployee.getRole());
+const nextActionInquirer = async () => {
+  try {
+    const { nextAction } = await inquirer.prompt(questions.selectionQuestion);
+    return nextAction;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const nextActionDecider = async () => {
+  try {
+    do {
+      nextAction = await nextActionInquirer();
+
+      switch (nextAction) {
+        case 'engineer':
+          await addEngineerInquirer();
+          break;
+        case 'intern':
+          await addInternInquirer();
+          break;
+      }
+    } while (nextAction !== 'finish');
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const init = async () => {
+  await addManagerInquirer();
+  await nextActionDecider();
+  console.log(team);
+};
+
+init();
--- src/questions.js
+++ src/questions.js
@@ -1,15 +1,50 @@
 const managerQuestions = [
-  { message: `Please type the team manager's name:`, type: 'input', name: 'managerName' },
-  { message: `Please type the team manager's employee ID:`, type: 'input', name: 'managerID' },
-  { message: `Please type the team manager's email:`, type: 'input', name: 'managerEmail' },
+  { message: `Please type the team manager's name:`, type: 'input', name: 'name' },
+  { message: `Please type the team manager's employee ID:`, type: 'input', name: 'id' },
+  { message: `Please type the team manager's email:`, type: 'input', name: 'email' },
   {
     message: `Please type the team manager's office number:`,
     type: 'input',
-    name: 'managerOffice',
+    name: 'officeNumber',
   },
-  { message: 'Please type THE USAGE section:', type: 'input', name: 'usage' },
 ];
 
+const engineerQuestions = [
+  { message: `Please type the new engineer's name:`, type: 'input', name: 'name' },
+  { message: `Please type the new engineer's employee ID:`, type: 'input', name: 'id' },
+  { message: `Please type the new engineer's email:`, type: 'input', name: 'email' },
+  {
+    message: `Please type the new engineer's github username:`,
+    type: 'input',
+    name: 'github',
+  },
+];
+
+const internQuestions = [
+  { message: `Please type the new intern's name:`, type: 'input', name: 'name' },
+  { message: `Please type the new intern's employee ID:`, type: 'input', name: 'id' },
+  { message: `Please type the new intern's email:`, type: 'input', name: 'email' },
+  {
+    message: `Please type the new intern's school name:`,
+    type: 'input',
+    name: 'school',
+  },
+];
+
+const selectionQuestion = {
+  message: 'What would you like to do next?',
+  type: 'list',
+  name: 'nextAction',
+  choices: [
+    { name: 'Add Engineer', value: 'engineer' },
+    { name: 'Add Intern', value: 'intern' },
+    { name: 'Finish', value: 'finish' },
+  ],
+};
+
 module.exports = {
+  selectionQuestion,
   managerQuestions,
+  engineerQuestions,
+  internQuestions,
 };
